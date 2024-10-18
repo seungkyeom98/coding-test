c1045379de5937f301b32ad4a62d144e64fff892
seungkyeom98 <kyeom9898@gmail.com>
2024-10-18 15:00:47 +0900

    [level 0] Title: 세균 증식, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = n;
+        int k=n;
+        for(int i=1; i<=t; i++){
+            
+            answer=answer*2;
+            
+        }
+        
+        return answer;
+    }
+}
