93cdf893adb187c5ae74ce89ef1ae28e1d27a87e
seungkyeom98 <kyeom9898@gmail.com>
2024-10-23 11:13:37 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    n= str(n)
+    
+    for i in n:
+        answer+=int(i)
+    
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return answer
