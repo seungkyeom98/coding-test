09743cb73655cd0099663322ec160e82b7465ca6
seungkyeom98 <kyeom9898@gmail.com>
2024-11-03 20:49:45 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,7 +1,11 @@
 def solution(s):
+
+    answer=''	#문자열 시퀀스
+    numList=[]	#list 시퀀스
     # 문자열을 공백 기준으로 분리하여 정수형 리스트로 변환
     numList = list(map(int, s.split()))
     # 문자열.split() 함수는 문자열을 일정한 규칙으로 잘라서 리스트로 만들어 주는 함수
+    # map함수 개꿀. for문 안쓰고, list.append()안해도 list화 쌉가능
     
     # 최소값과 최대값을 찾아 문자열 형식으로 반환
     answer = f"{min(numList)} {max(numList)}"
