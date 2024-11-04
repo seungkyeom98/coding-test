901681cd8d736fc06634a416bf9535513ff01cd0
seungkyeom98 <kyeom9898@gmail.com>
2024-11-05 08:43:04 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -5,8 +5,9 @@ def solution(A,B):
     A.sort()
     B.sort(reverse=True)
 
-    answer = list(map(multiply,A,B))
-    print(answer)
-    print(sum(answer))
+    # answer = list(map(multiply,A,B))
+    answer = map(multiply,A,B)
+    # print(answer)
+    # print(sum(answer))
 
     return sum(answer)
