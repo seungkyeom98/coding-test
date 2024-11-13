8b415fa363d55b16fe8308dcf2732d90d1f851dd
seungkyeom98 <kyeom9898@gmail.com>
2024-11-13 09:09:43 +0900

    [level 2] Title: N개의 최소공배수, Time: 34.98 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,33 +1,9 @@
-# from functools import reduce
-def gcd(a, b):  # 최대공약수
-    while b > 0:
-        a,b = b, a % b
-    return a
+from fractions import gcd
 
 
-# def lcm(a, b): #최대공배수
-#     return a * b / gcd(a, b) 
+def solution(num):      
+    answer = num[0]
+    for n in num:
+        answer = n * answer / gcd(n, answer)
 
-# def solution(arr):
-#     # answer = 0
-# #     multiply=1
-# #     for i in arr:
-# #         multiply*=i
-# #     for i in range(max(arr), multiply + 1):
-# #         for j in arr:
-            
-# #         if i % a == 0 and i % b == 0:
-# #             answer= i
-# #             break
-           
-#     return reduce(lcm, arr)
-# from math import gcd
-from functools import reduce
-
-# 두 수의 최소공배수 계산 함수
-def lcm(a, b):
-    return a * b // gcd(a, b)
-
-# 리스트의 모든 원소에 대한 최소공배수를 구하는 solution 함수
-def solution(arr):
-    return reduce(lcm, arr)
+    return answer
